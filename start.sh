411b84eb52647a74be4f853dca0475035dbb0795
Tveerasingh86 <Tveerasingh@outlook.com>
2024-08-17 05:54:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,13 @@
-
+echo "Cloning Repo...."
+if [ -z $BRANCH ]
+then
+  echo "Cloning main branch...."
+  git clone https://github.com/Tveerasingh86/Auto-Forward-Bot-V2dev
+else
+  echo "Cloning $BRANCH branch...."
+  git clone https://github.com/Tveerasingh86/Auto-Forward-Bot-V2dev
+fi
+cd SadKidBGMZ/fwdbot 
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
